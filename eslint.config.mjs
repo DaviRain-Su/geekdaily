83e38cc75abf73bcea0b981784a96535303ae0ff
Davirain <davirain.yin@gmail.com>
2025-06-27 07:41:56 +0800

    fix build warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
